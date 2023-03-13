c5dbf50dcdd6239461ed9ca049b69b01e12fb55d
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-13 15:52:06 -0300

    rendering a array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 
 export function List() {
+    const tasks =  [{
+        task: 'React',
+        time: '02:00:00'
+    }, {
+        task : 'Js',
+        time: '01:10:00'
+    }, {
+        task: 'Typescript',
+        time: '01:30:00'
+    }]
+
     return(
         <aside>
             <h2>Day's study</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Js
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.task} </h3>
+                        <span> {item.time} </span>
+                    </li>
+                ))}
             </ul>
         </aside>
     )
