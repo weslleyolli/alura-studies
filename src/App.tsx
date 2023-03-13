0a0c31e99646c92977ce5d8b5dadf406e05f5a23
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-13 15:37:53 -0300

    creating a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Forms from './components/Forms';
+import { List } from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Forms />
+      <List />
     </div>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function List() {
+    return(
+        <aside>
+            <h2>Day's study</h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Js
+                    </h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
