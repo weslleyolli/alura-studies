0db8491d3966e68851033c67a7304865a89b9723
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-17 23:16:21 -0300

    regressive

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,13 @@ import style from './Button.module.scss'
 
 class Button extends React.Component <{
      text: string
-     type?: 'button' | 'submit' | 'reset' | undefined
+     type?: 'button' | 'submit' | 'reset' | undefined,
+     onClick?: () => void
 }> {
     render() {
-        const { type = 'button' } = this.props
+        const { type = 'button', onClick } = this.props
         return (
-            <button type={type} className={style.button}>
+            <button onClick={onClick} type={type} className={style.button}>
                 {this.props.text}
             </button>
         )
--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -17,13 +17,23 @@ export default function Stopwatch({ selected }: Props) {
             setTime(timeToSecond(selected.time))
         }
     },[selected])
+
+    function regressive(cont: number = 0) {
+        setTimeout(() => {
+            if(cont > 0) {
+                setTime(cont - 1)
+                return regressive(cont - 1) 
+            }
+        }, 1000)
+    }
+
     return (
         <div className={style.stopwatch}>
             <p className={style.title}>Select a card and start the stopwatch</p>
             <div className={style.clockWrapper}>
                 <Clock time={time} />
             </div>
-            <Button 
+            <Button onClick={() => regressive(time)} 
                 text="start"
             />
         </div>
